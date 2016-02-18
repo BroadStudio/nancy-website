bd2ae22d00dca135474c3e213e1b66f1d839240f
Pat East <pat.east.design@gmail.com>
2016-02-18 22:49:01 +0000

    Adding basic theme boilerplate

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="footer" role="contentinfo">
+	<div class="copyright">
+		<div class="wrap">
+			<p>&copy;<?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+		</div>
+	</div>
+</footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7 oldie" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8 oldie" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 9]>
+<html class="ie ie9" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8) | !(IE 9)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+	<title><?php echo get_page_title(); ?></title>
+	<?php wp_head(); ?>
+</head>
