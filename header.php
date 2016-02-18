f263205f054e62ba25dbdfb75b1c78b2c0812e43
Pat East <pat.east.design@gmail.com>
2016-02-18 22:54:40 +0000

    Adding basic template parts

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+	<!-- <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" /> -->
 	<title><?php echo get_page_title(); ?></title>
 	<?php wp_head(); ?>
 </head>
