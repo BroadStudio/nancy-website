88d28439e39027116e99504462a0b8b865d5c575
Pat East <pat.east.design@gmail.com>
2016-02-19 23:51:54 +0000

    Making testimonial widget draw excerpts at random from Testimonial custom post type

--- template-parts/testimonial-widget.php
+++ template-parts/testimonial-widget.php
@@ -1,5 +1,24 @@
-<aside role="complimentary" class="testimonial-widget">
+<?php
+
+$args = array(
+    'post_type' => 'testimonials',
+    'orderby' => 'rand',
+    'posts_per_page' => 1,
+    );
+
+$the_query = new WP_Query( $args );
+
+if ( $the_query->have_posts() ) {
+	echo '<aside role="complimentary" class="testimonial-widget">';
+	while ( $the_query->have_posts() ) {
+		$the_query->the_post();
+		echo '<blockquote>' . get_the_excerpt() . '</blockquote>';
+	}
+	echo '<a class="btn" href="#">Read More</a>';
+	echo '</aside>';
+}
+/* Restore original Post Data */
+wp_reset_postdata();
+
+?>
     
-    <blockquote>Without Nancy’s help, our marriage of 21 years was headed downhill fast. She was able to help us find and understand the real issues behind the problems we were having, and we are learning how to overcome these issues now.</blockquote>
-    <a class="btn" href="#">Read More</a>
-</aside>
